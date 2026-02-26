5e9d441c3696f7233083cc7bd84b970c9262b4ff
Dosbol Abdikarim <a.dosbol06@gmail.com>
2026-02-26 07:51:12 +0500

    lab5

---
+++ web/lab5/src/app/model/product.model.ts
@@ -0,0 +1,12 @@
+export interface productModel {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    rating: number,
+    image: string,
+    images: string[],
+    link: string,
+    likes: number,
+    categoryId: number,
+}
